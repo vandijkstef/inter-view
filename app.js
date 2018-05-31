d91c2e5abfc6bb5582ceca3799b511aa4d9d5b5d
Stef van Dijk <stef@vandijkstef.nl>
2018-05-31 14:19:16 +0200

    Skkrrrra!

---
+++ app.js
@@ -0,0 +1,51 @@
+const createError = require('http-errors');
+const express = require('express');
+const session = require('express-session');
+const filestore = require('session-file-store')(session);
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const sassMiddleware = require('node-sass-middleware');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({
+	secret: 'keyboard cat',
+	store: new filestore(),
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: process.env.NODE_ENV ? true : false } // TODO: Set true on HTTPS env
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+	src: path.join(__dirname, 'public'),
+	dest: path.join(__dirname, 'public'),
+	indentedSyntax: false, // true = .sass and false = .scss
+	sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+	  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+router.get('/api', function(req, res) {
+	res.json({hello: 'world'});
+});
+
+module.exports = router;
