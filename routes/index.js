7100fe3646c0cd5b01055b0474265e622c116e1a
Stef van Dijk <stef@vandijkstef.nl>
2018-06-02 16:54:22 +0200

    Static serve HTML and JS transpiling

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	  res.sendFile(path.join(__dirname + '/public/index.html'));
+	console.log('yolos');
+	console.log(req.app.get('env'));
+	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 router.get('/api', function(req, res) {
