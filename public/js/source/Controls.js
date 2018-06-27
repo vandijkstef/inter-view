c1be33bdb489d0a7d6c01f1bffca10b2e41d7868
Stef van Dijk <stef@vandijkstef.nl>
2018-06-27 17:03:15 +0200

    Basic controls

--- public/js/source/Controls.js
+++ public/js/source/Controls.js
@@ -1,7 +1,17 @@
 export default class {
 	Keyboard(e) {
-		// console.log(e);
+		console.log(e);
 		// Determine app state
+		const interview = document.querySelector('form[name=interview]');
+		if (interview) {
+			switch (e.key) {
+			case 'ArrowRight':
+				console.log('next');
+				break;
+			case '1':
+				console.log(1);
+			}
+		}
 		// Act where applicable
 	}
 }
